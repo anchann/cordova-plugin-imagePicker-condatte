115938ffd07cbcf4e58d3dc9e23d85d273f2ae69
anchann <anchannnnnnnn@gmail.com>
2018-10-17 10:25:37 +0900

    Importing from app codebase

---
+++ src/android/com/synconset/ImagePicker/ImagePicker.java
@@ -0,0 +1,125 @@
+/**
+ * An Image Picker Plugin for Cordova/PhoneGap.
+ */
+package com.synconset;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+
+import org.apache.cordova.PluginResult;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+
+public class ImagePicker extends CordovaPlugin {
+
+	private static final String ACTION_GET_PICTURES = "getPictures";
+	private static final String ACTION_HAS_READ_PERMISSION = "hasReadPermission";
+	private static final String ACTION_REQUEST_READ_PERMISSION = "requestReadPermission";
+
+	private static final int PERMISSION_REQUEST_CODE = 100;
+
+	private CallbackContext callbackContext;
+
+	public boolean execute(String action, final JSONArray args, final CallbackContext callbackContext) throws JSONException {
+		 this.callbackContext = callbackContext;
+
+		if (ACTION_HAS_READ_PERMISSION.equals(action)) {
+			callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, hasReadPermission()));
+			return true;
+
+		} else if (ACTION_REQUEST_READ_PERMISSION.equals(action)) {
+			requestReadPermission();
+			return true;
+
+		} else if (ACTION_GET_PICTURES.equals(action)) {
+			final JSONObject params = args.getJSONObject(0);
+			final Intent imagePickerIntent = new Intent(cordova.getActivity(), MultiImageChooserActivity.class);
+			int max = 20;
+			int desiredWidth = 0;
+			int desiredHeight = 0;
+			int quality = 100;
+			if (params.has("maximumImagesCount")) {
+				max = params.getInt("maximumImagesCount");
+			}
+			if (params.has("width")) {
+				desiredWidth = params.getInt("width");
+			}
+			if (params.has("height")) {
+				desiredHeight = params.getInt("height");
+			}
+			if (params.has("quality")) {
+				quality = params.getInt("quality");
+			}
+			imagePickerIntent.putExtra("MAX_IMAGES", max);
+			imagePickerIntent.putExtra("WIDTH", desiredWidth);
+			imagePickerIntent.putExtra("HEIGHT", desiredHeight);
+			imagePickerIntent.putExtra("QUALITY", quality);
+			if (hasReadPermission()) {
+				cordova.startActivityForResult(this, imagePickerIntent, 0);
+			} else {
+				requestReadPermission();
+				// The downside is the user needs to re-invoke this picker method.
+				// The best thing to do for the dev is check 'hasReadPermission' manually and
+				// run 'requestReadPermission' or 'getPictures' based on the outcome.
+			}
+			return true;
+		}
+		return false;
+	}
+
+	private boolean hasReadPermission() {
+		return Build.VERSION.SDK_INT < 23 ||
+			PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(this.cordova.getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE);
+	}
+
+	private void requestReadPermission() {
+		if (!hasReadPermission()) {
+			ActivityCompat.requestPermissions(
+				this.cordova.getActivity(),
+				new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},
+				PERMISSION_REQUEST_CODE);
+		}
+		// This method executes async and we seem to have no known way to receive the result
+		// (that's why these methods were later added to Cordova), so simply returning ok now.
+		callbackContext.success();
+	}
+
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (resultCode == Activity.RESULT_OK && data != null) {
+			ArrayList<String> fileNames = data.getStringArrayListExtra("MULTIPLEFILENAMES");
+			JSONArray res = new JSONArray(fileNames);
+			this.callbackContext.success(res);
+		} else if (resultCode == Activity.RESULT_CANCELED && data != null) {
+			String error = data.getStringExtra("ERRORMESSAGE");
+			this.callbackContext.error(error);
+		} else if (resultCode == Activity.RESULT_CANCELED) {
+			JSONArray res = new JSONArray();
+			this.callbackContext.success(res);
+		} else {
+			this.callbackContext.error("No images selected");
+		}
+	}
+
+	/**
+	 * Choosing a picture launches another Activity, so we need to implement the
+	 * save/restore APIs to handle the case where the CordovaActivity is killed by the OS
+	 * before we get the launched Activity's result.
+	 *
+	 * @see http://cordova.apache.org/docs/en/dev/guide/platforms/android/plugin.html#launching-other-activities
+	 */
+	public void onRestoreStateForActivityResult(Bundle state, CallbackContext callbackContext) {
+		this.callbackContext = callbackContext;
+	}
+}
